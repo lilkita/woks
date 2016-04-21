0a9897eaccd1be055e3a873bfc6086c756cfb235
Krista Leigh <kleigh617@gmail.com>
2016-04-21 18:30:31 +0000

    Validated address, name length, and description

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -33,12 +33,14 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render text: "Not Allowed", status: :forbidden
     end
+
     @place.update_attributes(place_params)
     if @place.valid?
-    redirect_to root_path
+      redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
