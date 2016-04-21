e9c07a47ac4a684afaca3332e274e3666513472a
Krista Leigh <kleigh617@gmail.com>
2016-04-21 17:30:01 +0000

    Added validation to edit and update forms if name field is left blank

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+    @place = current_user.places.create(place_params)
+    if @place.valid?
     redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -32,9 +36,12 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: "Not Allowed", status: :forbidden
     end
-
     @place.update_attributes(place_params)
+    if @place.valid?
     redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -42,7 +49,7 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: "Not Allowed", status: :forbidden
     end
-    
+
     @place.destroy
     redirect_to root_path
   end
